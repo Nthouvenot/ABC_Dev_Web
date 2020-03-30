b61b377ef382f3021b4df4c67ca3b999dcfe211a
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-03-30 08:06:49 +0200

    Travail du 27/03/2020

--- JS/Exercices/PremiersEffetsChangementPolice/script.js
+++ JS/Exercices/PremiersEffetsChangementPolice/script.js
@@ -1,15 +1,23 @@
 var paragraphe = document.getElementsByTagName('p');
 paragraphe[0].onmouseover = changeParagraphe;
 paragraphe[0].onmouseout = origineParagraphe;
-paragraphe[0].style = 'width: 500px;'
+paragraphe[0].style.fontFamily = 'Verdana';
 
 function changeParagraphe() {
-    paragraphe[0].style.fontFamily = 'Verdana';
+    var text='', tmp='';
+    var array1=[];
+    text = paragraphe[0].innerHTML;
+    array1 = text.split(' ');
+    for (i = 0; i < array1.length; i++) {
+        array1[i] = array1[i].charAt(0).toUpperCase();
+    }
     paragraphe[0].style.fontSize = '2em';
-    paragraphe[0].style.fontStyle = ''
+    paragraphe[0].style.textDecoration = 'line-through'
+   
 }
 
 function origineParagraphe() {
-    paragraphe[0].style.fontFamily = 'SansSerif';
     paragraphe[0].style.fontSize = '1em';
+    paragraphe[0].style.textDecoration = 'none';
+
 }
