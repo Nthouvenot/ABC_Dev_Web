82827bbf1d81a993aac494cbf1877ce7830d711c
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-04-09 18:24:20 +0200

    Travail du 09/04/2020

---
+++ PHP/Exemples/Base POO/accederElementThis2.php
@@ -0,0 +1,28 @@
+<?php
+class Personnage
+{
+  private $_degats; // Les dégâts du personnage.
+  private $_experience; // L'expérience du personnage.
+  private $_force; // La force du personnage (plus elle est grande, plus l'attaque est puissante).
+        
+  public function frapper($persoAFrapper)
+  {
+    $persoAFrapper->_degats += $this->_force;
+  }
+        
+  public function gagnerExperience()
+  {
+    // On ajoute 1 à notre attribut $_experience.
+    $this->_experience = $this->_experience + 1;
+  }
+}
+
+$perso1 = new Personnage; // Un premier personnage
+$perso2 = new Personnage; // Un second personnage
+
+$perso1->frapper($perso2); // $perso1 frappe $perso2
+$perso1->gagnerExperience(); // $perso1 gagne de l'expérience
+
+$perso2->frapper($perso1); // $perso2 frappe $perso1
+$perso2->gagnerExperience(); // $perso2 gagne de l'expérience
+?>
